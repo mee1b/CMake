daf8e333a4fe7645a214f747c9f89cc9285fd807
mee1b <mee1b@yandex.ru>
2024-10-28 20:02:46 +0900

    Initial commit

---
+++ my_first_cmake_project/my_first_cmake_project/my_first_cmake_project.cpp
@@ -0,0 +1,33 @@
+﻿// my_first_cmake_project.cpp: определяет точку входа для приложения.
+//
+
+#include "my_first_cmake_project.h"
+
+class WhatIsYourName
+{
+private:
+	std::string name;
+	void print_your_name() const
+	{
+		std::cout << "Здравствуйте, " << name << std::endl;
+	}
+public:
+	WhatIsYourName(std::string name)
+	{
+		this->name = name;
+		print_your_name();
+	}
+};
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	std::string name{};
+	std::cout << "Введите ваше имя: ";
+	std::cin >> name;
+	WhatIsYourName w (name);
+	
+	return EXIT_SUCCESS;
+}
